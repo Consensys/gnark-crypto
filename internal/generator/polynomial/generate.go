8b5530fc18e4b28ec601302c438a9a8f47e92a71
Gautam Botrel <gautam.botrel@gmail.com>
2021-04-02 17:09:56 -0500

    refactor: ported mock commitment scheme from gnark

--- internal/generator/polynomial/generate.go
+++ internal/generator/polynomial/generate.go
@@ -11,5 +11,18 @@ func Generate(conf config.Curve, baseDir string, bgen *bavard.BatchGenerator) er
 	entries := []bavard.EntryF{
 		{File: filepath.Join(baseDir, "polynomial.go"), TemplateF: []string{"polynomial.go.tmpl"}},
 	}
-	return bgen.GenerateF(conf, "polynomial", "./polynomial/template/", entries...)
+	if err := bgen.GenerateF(conf, "polynomial", "./polynomial/template/", entries...); err != nil {
+		return err
+	}
+
+	// mock commitment scheme
+	entries = []bavard.EntryF{
+		{File: filepath.Join(baseDir, "mockcommitment", "digest.go"), TemplateF: []string{"commitment_mock/digest.go.tmpl"}},
+		{File: filepath.Join(baseDir, "mockcommitment", "proof.go"), TemplateF: []string{"commitment_mock/proof.go.tmpl"}},
+		{File: filepath.Join(baseDir, "mockcommitment", "proof_single_point.go"), TemplateF: []string{"commitment_mock/proof.single.point.go.tmpl"}},
+		{File: filepath.Join(baseDir, "mockcommitment", "scheme.go"), TemplateF: []string{"commitment_mock/scheme.go.tmpl"}},
+	}
+
+	return bgen.GenerateF(conf, "mockcommitment", "./polynomial/template/", entries...)
+
 }
