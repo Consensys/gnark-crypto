f985792369b0d6d89c9635c784e38fc95c4f7384
Gautam Botrel <gautam.botrel@gmail.com>
2021-04-05 18:16:16 -0500

    docs: added fft package level doc

--- internal/generator/fft/generate.go
+++ internal/generator/fft/generate.go
@@ -8,11 +8,12 @@ import (
 )
 
 func Generate(conf config.Curve, baseDir string, bgen *bavard.BatchGenerator) error {
+	doc := "provides in-place discrete Fourier transform"
 	entries := []bavard.EntryF{
 		{File: filepath.Join(baseDir, "domain_test.go"), TemplateF: []string{"tests/domain.go.tmpl", "imports.go.tmpl"}},
 		{File: filepath.Join(baseDir, "domain.go"), TemplateF: []string{"domain.go.tmpl", "imports.go.tmpl"}},
 		{File: filepath.Join(baseDir, "fft_test.go"), TemplateF: []string{"tests/fft.go.tmpl", "imports.go.tmpl"}},
-		{File: filepath.Join(baseDir, "fft.go"), TemplateF: []string{"fft.go.tmpl", "imports.go.tmpl"}},
+		{File: filepath.Join(baseDir, "fft.go"), TemplateF: []string{"fft.go.tmpl", "imports.go.tmpl"}, PackageDoc: doc},
 	}
 	return bgen.GenerateF(conf, "fft", "./fft/template/", entries...)
 }
