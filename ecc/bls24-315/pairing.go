27c7646c820e38b86c9dabd2e827f8219e3d05be
Youssef El Housni <youssef.housni21@gmail.com>
2021-04-29 14:34:50 +0000

    style: remove bls24 code gen + fmt

--- ecc/bls24-315/pairing.go
+++ ecc/bls24-315/pairing.go
@@ -69,43 +69,43 @@ func FinalExponentiation(z *GT, _z ...*GT) GT {
 	result.FrobeniusQuad(&t[0]).
 		Mul(&result, &t[0])
 
-    // hard part
-    t[0].Expt(&result)
-    t[1].Expt(&t[0])
-    t[0].InverseUnitary(&t[0])
-    t[0].CyclotomicSquare(&t[0])
-    t[0].Mul(&t[0], &t[1])
-    t[0].Mul(&t[0], &result)
-    t[1].Expt(&t[0])
-    t[2].Expt(&t[1])
-    t[3].Expt(&t[2])
-    t[4].Expt(&t[3])
-    t[5].InverseUnitary(&t[0])
-    t[4].Mul(&t[4], &t[5])
-    t[5].Expt(&t[4])
-    t[6].Expt(&t[5])
-    t[7].Expt(&t[6])
-    t[8].CyclotomicSquare(&result)
-    t[8].Mul(&result, &t[8])
-    t[7].Mul(&t[7], &t[8])
+	// hard part
+	t[0].Expt(&result)
+	t[1].Expt(&t[0])
+	t[0].InverseUnitary(&t[0])
+	t[0].CyclotomicSquare(&t[0])
+	t[0].Mul(&t[0], &t[1])
+	t[0].Mul(&t[0], &result)
+	t[1].Expt(&t[0])
+	t[2].Expt(&t[1])
+	t[3].Expt(&t[2])
+	t[4].Expt(&t[3])
+	t[5].InverseUnitary(&t[0])
+	t[4].Mul(&t[4], &t[5])
+	t[5].Expt(&t[4])
+	t[6].Expt(&t[5])
+	t[7].Expt(&t[6])
+	t[8].CyclotomicSquare(&result)
+	t[8].Mul(&result, &t[8])
+	t[7].Mul(&t[7], &t[8])
 
-    t[6].Frobenius(&t[6])
-    t[5].FrobeniusSquare(&t[5])
-    t[4].FrobeniusCube(&t[4])
-    t[3].FrobeniusQuad(&t[3])
-    t[2].FrobeniusFive(&t[2])
-    t[1].FrobeniusSix(&t[1])
-    t[0].FrobeniusSeven(&t[0])
+	t[6].Frobenius(&t[6])
+	t[5].FrobeniusSquare(&t[5])
+	t[4].FrobeniusCube(&t[4])
+	t[3].FrobeniusQuad(&t[3])
+	t[2].FrobeniusFive(&t[2])
+	t[1].FrobeniusSix(&t[1])
+	t[0].FrobeniusSeven(&t[0])
 
-    result.Mul(&t[7], &t[6]).
-        Mul(&result, &t[5]).
-        Mul(&result, &t[4]).
-        Mul(&result, &t[3]).
-        Mul(&result, &t[2]).
-        Mul(&result, &t[1]).
-        Mul(&result, &t[0])
+	result.Mul(&t[7], &t[6]).
+		Mul(&result, &t[5]).
+		Mul(&result, &t[4]).
+		Mul(&result, &t[3]).
+		Mul(&result, &t[2]).
+		Mul(&result, &t[1]).
+		Mul(&result, &t[0])
 
-    return result
+	return result
 }
 
 // MillerLoop Miller loop
