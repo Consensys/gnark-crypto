964285567b936f719cd909ac0d7c4ec6270e2c22
Thomas Piellard <thomas.piellard@consensys.net>
2021-10-04 12:06:48 +0200

    feat: code gen for polynomials

--- ecc/bls24-315/fr/polynomial/polynomial.go
+++ ecc/bls24-315/fr/polynomial/polynomial.go
@@ -17,107 +17,195 @@
 package polynomial
 
 import (
+	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark-crypto/ecc/bls24-315/fr"
+	"github.com/consensys/gnark-crypto/ecc/bls24-315/fr/fft"
 )
 
-// Polynomial polynomial represented by coefficients bn254 fr field.
+// Polynomial polynomial represented by coefficients bls24-315 fr field.
 type Polynomial []fr.Element
 
-// Degree returns the degree of the polynomial, which is the length of Data.
-func (p *Polynomial) Degree() uint64 {
-	return uint64(len(*p) - 1)
+// NewPolynomial
+func NewPolynomial(size uint64) Polynomial {
+	s := ecc.NextPowerOfTwo(size)
+	res := make(Polynomial, size, s)
+	return res
 }
 
 // Eval evaluates p at v
 // returns a fr.Element
 func (p *Polynomial) Eval(v *fr.Element) fr.Element {
 
-	res := (*p)[len(*p)-1]
-	for i := len(*p) - 2; i >= 0; i-- {
+	_p := *p
+	res := _p[len(_p)-1]
+	for i := len(_p) - 2; i >= 0; i-- {
 		res.Mul(&res, v)
-		res.Add(&res, &(*p)[i])
+		res.Add(&res, &_p[i])
 	}
 
 	return res
 }
 
-// Clone returns a copy of the polynomial
-func (p *Polynomial) Clone() Polynomial {
+// Copy returns a copy of the polynomial
+func (p *Polynomial) Copy() *Polynomial {
 	_p := make(Polynomial, len(*p))
 	copy(_p, *p)
-	return _p
+	return &_p
 }
 
-// AddConstantInPlace adds a constant to the polynomial, modifying p
-func (p *Polynomial) AddConstantInPlace(c *fr.Element) {
-	for i := 0; i < len(*p); i++ {
-		(*p)[i].Add(&(*p)[i], c)
+// sort returns pi, pj where len(pi)>=len(pj).
+func sort(p1, p2 *Polynomial) (Polynomial, Polynomial) {
+	s1 := len(*p1)
+	s2 := len(*p2)
+	if s2 > s1 {
+		return *p2, *p1
 	}
+	return *p1, *p2
 }
 
-// SubConstantInPlace subs a constant to the polynomial, modifying p
-func (p *Polynomial) SubConstantInPlace(c *fr.Element) {
-	for i := 0; i < len(*p); i++ {
-		(*p)[i].Sub(&(*p)[i], c)
+// resize adapt the size of p to max(len(p1),len(p2),len(p))
+func (p *Polynomial) resize(p1, p2 *Polynomial) {
+	if len(*p) > len(*p1) && len(*p) > len(*p2) {
+		return
 	}
+	_p1, _ := sort(p1, p2)
+	pad(p, len(_p1)-len(*p))
 }
 
-// ScaleInPlace multiplies p by v, modifying p
-func (p *Polynomial) ScaleInPlace(c *fr.Element) {
-	for i := 0; i < len(*p); i++ {
-		(*p)[i].Mul(&(*p)[i], c)
-	}
-}
-
-// Add adds p1 to p2
-// This function allocates a new slice unless p == p1 or p == p2
-func (p *Polynomial) Add(p1, p2 Polynomial) *Polynomial {
-
-	bigger := p1
-	smaller := p2
-	if len(bigger) < len(smaller) {
-		bigger, smaller = smaller, bigger
-	}
-
-	if len(*p) == len(bigger) && (&(*p)[0] == &bigger[0]) {
-		for i := 0; i < len(smaller); i++ {
-			(*p)[i].Add(&(*p)[i], &smaller[i])
+// Degree returns the degree of p
+func (p *Polynomial) Degree() uint64 {
+	_p := *p
+	res := len(_p) - 1
+	for i := len(_p) - 1; i >= 0; i-- {
+		if !_p[i].IsZero() {
+			return uint64(res)
 		}
-		return p
+		res--
 	}
-
-	if len(*p) == len(smaller) && (&(*p)[0] == &smaller[0]) {
-		for i := 0; i < len(smaller); i++ {
-			(*p)[i].Add(&(*p)[i], &bigger[i])
-		}
-		*p = append(*p, bigger[len(smaller):]...)
-		return p
-	}
-
-	res := make(Polynomial, len(bigger))
-	copy(res, bigger)
-	for i := 0; i < len(smaller); i++ {
-		res[i].Add(&res[i], &smaller[i])
-	}
-	*p = res
-	return p
+	return uint64(res)
 }
 
 // Equal checks equality between two polynomials
-func (p *Polynomial) Equal(p1 Polynomial) bool {
-	if (*p == nil) != (p1 == nil) {
-		return false
+func (p *Polynomial) Equal(p1 *Polynomial) bool {
+
+	res := true
+	_p1, _p2 := sort(p, p1)
+	s1 := len(_p1)
+	s2 := len(_p2)
+	d := s1 - s2
+	for i := 0; i < d; i++ {
+		res = res && _p1[s1-1-i].IsZero()
+	}
+	for i := 0; i < s2; i++ {
+		res = res && _p1[s2-1-i].Equal(&_p2[s2-1-i])
 	}
 
-	if len(*p) != len(p1) {
-		return false
-	}
+	return res
+}
 
-	for i := range p1 {
-		if !(*p)[i].Equal(&p1[i]) {
-			return false
-		}
+// addSorted  sets p to p1+p2, assuming len(p)>=len(p1)>=len(p2)
+func addSorted(p, p1, p2 *Polynomial) {
+	i := 0
+	for ; i < len(*p2); i++ {
+		(*p)[i].Add(&(*p1)[i], &(*p2)[i])
 	}
+	for ; i < len(*p1); i++ {
+		(*p)[i].Add(&(*p1)[i], &(*p2)[i])
+	}
+	for ; i < len(*p); i++ {
+		(*p)[i].SetZero()
+	}
+}
 
-	return true
+// subSorted sets p to p1-p2, assuming len(p)>=len(p1)>=len(p2)
+func subSorted(p, p1, p2 *Polynomial) {
+	i := 0
+	for ; i < len(*p2); i++ {
+		(*p)[i].Sub(&(*p1)[i], &(*p2)[i])
+	}
+	for ; i < len(*p1); i++ {
+		(*p)[i].Sub(&(*p1)[i], &(*p2)[i])
+	}
+	for ; i < len(*p); i++ {
+		(*p)[i].SetZero()
+	}
+}
+
+// Add adds p1 to p, and return p
+func (p *Polynomial) Add(p1, p2 *Polynomial) *Polynomial {
+	_p1, _p2 := sort(p1, p2)
+	p.resize(p1, p2)
+	addSorted(p, &_p1, &_p2)
+	return p
+}
+
+// Sub adds p1 to p, and return p
+func (p *Polynomial) Sub(p1, p2 *Polynomial) *Polynomial {
+	_p1, _p2 := sort(p1, p2)
+	p.resize(p1, p2)
+	subSorted(p, &_p1, &_p2)
+	return p
+}
+
+// Neg sets p to -p1
+func (p *Polynomial) Neg(p1 *Polynomial) *Polynomial {
+	p.resize(p1, p)
+	*p = *p1
+	for i := 0; i < len(*p); i++ {
+		(*p)[i].Neg(&(*p)[i])
+	}
+	return p
+}
+
+// SetZero sets p=zero(p1)
+func (p *Polynomial) SetZero() *Polynomial {
+	for i := 0; i < len(*p); i++ {
+		(*p)[i].SetZero()
+	}
+	return p
+}
+
+// pad pads p with i zeros
+func pad(p *Polynomial, i int) {
+	pad := make(Polynomial, i)
+	*p = append(*p, pad...)
+}
+
+// mul sets p1=p2*p3, assuming all polynomials and the domain
+// have the same size.
+func mul(p1, p2, p3 *Polynomial) {
+	p1.SetZero()
+	d := fft.NewDomain(uint64(len(*p1)), 0, false)
+	d.FFT(*p2, fft.DIF, 0)
+	d.FFT(*p3, fft.DIF, 0)
+	for i := 0; i < len(*p1); i++ {
+		(*p1)[i].Mul(&(*p2)[i], &(*p3)[i])
+	}
+	d.FFTInverse(*p1, fft.DIT, 0)
+}
+
+// Mul sets p to p1*p2
+func (p *Polynomial) Mul(p1, p2 *Polynomial) *Polynomial {
+
+	_p1, _p2 := sort(p1, p2)
+	s := len(_p1)
+	n := ecc.NextPowerOfTwo(uint64(s))
+	d1 := _p1.Degree()
+	d2 := _p2.Degree()
+	if d1+d2 < n {
+		cp1 := make(Polynomial, n)
+		cp2 := make(Polynomial, n)
+		copy(cp1, _p1)
+		copy(cp2, _p2)
+		p.resize(&cp1, &cp2)
+		mul(p, &cp1, &cp2)
+		return p
+	}
+	cp1 := make(Polynomial, 2*n)
+	cp2 := make(Polynomial, 2*n)
+	copy(cp1, _p1)
+	copy(cp2, _p2)
+	p.resize(&cp1, &cp2)
+	mul(p, &cp1, &cp2)
+	return p
 }
--- ecc/bls24-315/fr/polynomial/polynomial_test.go
+++ ecc/bls24-315/fr/polynomial/polynomial_test.go
@@ -17,11 +17,229 @@
 package polynomial
 
 import (
+	"crypto/rand"
 	"math/big"
 	"testing"
 
 	"github.com/consensys/gnark-crypto/ecc/bls24-315/fr"
+	"github.com/leanovate/gopter"
+	"github.com/leanovate/gopter/prop"
 )
+
+type Op uint8
+
+const (
+	ADD Op = iota
+	SUB
+	MUL
+)
+
+// probabilisticCheck checks if c == Op(a, b) using Scwhartz Zippel
+func probabilisticCheck(a, b, c Polynomial, r fr.Element, op Op) bool {
+
+	ar := a.Eval(&r)
+	br := b.Eval(&r)
+	cr := c.Eval(&r)
+	switch op {
+	case ADD:
+		ar.Add(&ar, &br)
+		return ar.Equal(&cr)
+	case SUB:
+		ar.Sub(&ar, &br)
+		return ar.Equal(&cr)
+	case MUL:
+		ar.Mul(&ar, &br)
+		return ar.Equal(&cr)
+	default:
+		panic("operation not supported")
+	}
+}
+
+// GenFr generates an Fr element
+// TODO factor this, redeclared in marshal_test.go
+func GenFr() gopter.Gen {
+	return func(genParams *gopter.GenParameters) *gopter.GenResult {
+		var elmt fr.Element
+		var b [fr.Bytes]byte
+		_, err := rand.Read(b[:])
+		if err != nil {
+			panic(err)
+		}
+		elmt.SetBytes(b[:])
+		genResult := gopter.NewGenResult(elmt, gopter.NoShrinker)
+		return genResult
+	}
+}
+
+func randomPolynomial(size int) Polynomial {
+	res := NewPolynomial(uint64(size))
+	for i := 0; i < size; i++ {
+		res[i].SetRandom()
+	}
+	return res
+}
+
+func TestOperands(t *testing.T) {
+
+	parameters := gopter.DefaultTestParameters()
+	parameters.MinSuccessfulTests = 10
+
+	properties := gopter.NewProperties(parameters)
+
+	properties.Property("(ADD) check operands which are not receivers are not modified", prop.ForAll(
+		func(r fr.Element) bool {
+			res := true
+			{
+				a := randomPolynomial(4)
+				b := randomPolynomial(8)
+				ac := a.Copy()
+				bc := b.Copy()
+				var c Polynomial
+				c.Add(&a, &b)
+				res = res && ac.Equal(&a) && bc.Equal(&b)
+			}
+			{
+				a := randomPolynomial(4)
+				b := randomPolynomial(8)
+				ac := a.Copy()
+				b.Add(&a, &b)
+				res = res && ac.Equal(&a)
+			}
+			{
+				a := randomPolynomial(4)
+				b := randomPolynomial(8)
+				bc := b.Copy()
+				a.Add(&a, &b)
+				res = res && bc.Equal(&b)
+			}
+			return res
+		},
+		GenFr(),
+	))
+
+	properties.Property("(ADD) check operands which are not receivers are not modified", prop.ForAll(
+		func(r fr.Element) bool {
+			res := true
+			{
+				a := randomPolynomial(4)
+				b := randomPolynomial(8)
+				ac := a.Copy()
+				bc := b.Copy()
+				var c Polynomial
+				c.Sub(&a, &b)
+				res = res && ac.Equal(&a) && bc.Equal(&b)
+			}
+			{
+				a := randomPolynomial(4)
+				b := randomPolynomial(8)
+				ac := a.Copy()
+				b.Sub(&a, &b)
+				res = res && ac.Equal(&a)
+			}
+			{
+				a := randomPolynomial(4)
+				b := randomPolynomial(8)
+				bc := b.Copy()
+				a.Sub(&a, &b)
+				res = res && bc.Equal(&b)
+			}
+			return res
+		},
+		GenFr(),
+	))
+
+	properties.Property("(MUL) check operands which are not receivers are not modified", prop.ForAll(
+		func(r fr.Element) bool {
+			res := true
+			{
+				a := randomPolynomial(4)
+				b := randomPolynomial(8)
+				ac := a.Copy()
+				bc := b.Copy()
+				var c Polynomial
+				c.Mul(&a, &b)
+				res = res && ac.Equal(&a) && bc.Equal(&b)
+			}
+			{
+				a := randomPolynomial(4)
+				b := randomPolynomial(8)
+				ac := a.Copy()
+				b.Mul(&a, &b)
+				res = res && ac.Equal(&a)
+			}
+			{
+				a := randomPolynomial(4)
+				b := randomPolynomial(8)
+				bc := b.Copy()
+				a.Mul(&a, &b)
+				res = res && bc.Equal(&b)
+			}
+			return res
+		},
+		GenFr(),
+	))
+
+}
+
+func TestPolynomialOps(t *testing.T) {
+
+	parameters := gopter.DefaultTestParameters()
+	parameters.MinSuccessfulTests = 10
+
+	properties := gopter.NewProperties(parameters)
+
+	// size of polynomials [a,b,c] -> a = op(b, c)
+	configs := [][3]int{
+		{4, 8, 16},
+		{16, 16, 16},
+		{16, 8, 4},
+	}
+
+	properties.Property("p(a)+q(a)=(p+q)(a)", prop.ForAll(
+		func(r fr.Element) bool {
+			res := true
+			for _, conf := range configs {
+				a := randomPolynomial(conf[0])
+				b := randomPolynomial(conf[1])
+				c := NewPolynomial(uint64(conf[2]))
+				c.Add(&a, &b)
+				res = res && probabilisticCheck(a, b, c, r, ADD)
+			}
+			return res
+		},
+		GenFr(),
+	))
+
+	properties.Property("p(a)-q(a)=(p-q)(a)", prop.ForAll(
+		func(r fr.Element) bool {
+			res := true
+			for _, conf := range configs {
+				a := randomPolynomial(conf[0])
+				b := randomPolynomial(conf[1])
+				c := NewPolynomial(uint64(conf[2]))
+				c.Add(&a, &b)
+				res = res && probabilisticCheck(a, b, c, r, SUB)
+			}
+			return res
+		},
+		GenFr(),
+	))
+
+	properties.Property("p(a)*q(a)=(p*q)(a)", prop.ForAll(
+		func(r fr.Element) bool {
+			res := true
+			for _, conf := range configs {
+				a := randomPolynomial(conf[0])
+				b := randomPolynomial(conf[1])
+				c := NewPolynomial(uint64(conf[2]))
+				c.Add(&a, &b)
+				res = res && probabilisticCheck(a, b, c, r, MUL)
+			}
+			return res
+		},
+		GenFr(),
+	))
+}
 
 func TestPolynomialEval(t *testing.T) {
 
@@ -51,158 +269,5 @@ func TestPolynomialEval(t *testing.T) {
 	// check
 	if !purportedEval.Equal(&expectedEval) {
 		t.Fatal("polynomial evaluation failed")
-	}
-}
-
-func TestPolynomialAddConstantInPlace(t *testing.T) {
-
-	// build polynomial
-	f := make(Polynomial, 20)
-	for i := 0; i < 20; i++ {
-		f[i].SetOne()
-	}
-
-	// constant to add
-	var c fr.Element
-	c.SetRandom()
-
-	// add constant
-	f.AddConstantInPlace(&c)
-
-	// check
-	var expectedCoeffs, one fr.Element
-	one.SetOne()
-	expectedCoeffs.Add(&one, &c)
-	for i := 0; i < 20; i++ {
-		if !f[i].Equal(&expectedCoeffs) {
-			t.Fatal("AddConstantInPlace failed")
-		}
-	}
-}
-
-func TestPolynomialSubConstantInPlace(t *testing.T) {
-
-	// build polynomial
-	f := make(Polynomial, 20)
-	for i := 0; i < 20; i++ {
-		f[i].SetOne()
-	}
-
-	// constant to sub
-	var c fr.Element
-	c.SetRandom()
-
-	// sub constant
-	f.SubConstantInPlace(&c)
-
-	// check
-	var expectedCoeffs, one fr.Element
-	one.SetOne()
-	expectedCoeffs.Sub(&one, &c)
-	for i := 0; i < 20; i++ {
-		if !f[i].Equal(&expectedCoeffs) {
-			t.Fatal("SubConstantInPlace failed")
-		}
-	}
-}
-
-func TestPolynomialScaleInPlace(t *testing.T) {
-
-	// build polynomial
-	f := make(Polynomial, 20)
-	for i := 0; i < 20; i++ {
-		f[i].SetOne()
-	}
-
-	// constant to scale by
-	var c fr.Element
-	c.SetRandom()
-
-	// scale by constant
-	f.ScaleInPlace(&c)
-
-	// check
-	for i := 0; i < 20; i++ {
-		if !f[i].Equal(&c) {
-			t.Fatal("ScaleInPlace failed")
-		}
-	}
-
-}
-
-func TestPolynomialAdd(t *testing.T) {
-
-	// build unbalanced polynomials
-	f1 := make(Polynomial, 20)
-	f1Backup := make(Polynomial, 20)
-	for i := 0; i < 20; i++ {
-		f1[i].SetOne()
-		f1Backup[i].SetOne()
-	}
-	f2 := make(Polynomial, 10)
-	f2Backup := make(Polynomial, 10)
-	for i := 0; i < 10; i++ {
-		f2[i].SetOne()
-		f2Backup[i].SetOne()
-	}
-
-	// expected result
-	var one, two fr.Element
-	one.SetOne()
-	two.Double(&one)
-	expectedSum := make(Polynomial, 20)
-	for i := 0; i < 10; i++ {
-		expectedSum[i].Set(&two)
-	}
-	for i := 10; i < 20; i++ {
-		expectedSum[i].Set(&one)
-	}
-
-	// caller is empty
-	var g Polynomial
-	g.Add(f1, f2)
-	if !g.Equal(expectedSum) {
-		t.Fatal("add polynomials fails")
-	}
-	if !f1.Equal(f1Backup) {
-		t.Fatal("side effect, f1 should not have been modified")
-	}
-	if !f2.Equal(f2Backup) {
-		t.Fatal("side effect, f2 should not have been modified")
-	}
-
-	// all operands are distincts
-	_f1 := f1.Clone()
-	_f1.Add(f1, f2)
-	if !_f1.Equal(expectedSum) {
-		t.Fatal("add polynomials fails")
-	}
-	if !f1.Equal(f1Backup) {
-		t.Fatal("side effect, f1 should not have been modified")
-	}
-	if !f2.Equal(f2Backup) {
-		t.Fatal("side effect, f2 should not have been modified")
-	}
-
-	// first operand = caller
-	_f1 = f1.Clone()
-	_f2 := f2.Clone()
-	_f1.Add(_f1, _f2)
-	if !_f1.Equal(expectedSum) {
-		t.Fatal("add polynomials fails")
-	}
-	if !_f2.Equal(f2Backup) {
-		t.Fatal("side effect, _f2 should not have been modified")
-	}
-
-	// second operand = caller
-	_f1 = f1.Clone()
-	_f2 = f2.Clone()
-	_f1.Add(_f2, _f1)
-	if !_f1.Equal(expectedSum) {
-		t.Fatal("add polynomials fails")
-	}
-	if !_f2.Equal(f2Backup) {
-		t.Fatal("side effect, _f2 should not have been modified")
 	}
 }
