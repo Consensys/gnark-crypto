772377ac5289e6e29f2c65d5fad74be30c9c9068
Arya Tabaie <arya.pourtabatabaie@consensys.net>
2021-11-01 15:45:50 -0400

    perf: Field element - Word multiplication implemented

--- ecc/bn254/fp/pornin20_inv.go
+++ ecc/bn254/fp/pornin20_inv.go
@@ -1,46 +1,12 @@
 package fp
 
 import (
-	"math/big"
+	"math/bits"
 )
-
-func ComputeCorrectiveFactorPornin1() {
-	numIterations := 2*Bits - 2
-	var fac Element
-	fac.SetInt64(2)
-	fac.Exp(fac, big.NewInt(int64(-numIterations)))
-	fac.Mul(&fac, &rSquare)
-
-}
-
-//Three possible ways to swap values:
-//	1. Swap by value :D
-//	2. Swap by reference
-//	3. Run all assignments using conditionals on whether a or b is smaller
-func swapBigInt(a *big.Int, b *big.Int, swapScratch *big.Int) {
-	swapScratch.Set(a)
-	a.Set(b)
-	b.Set(swapScratch)
-}
-
-func (z *Element) swap(a *Element, b *Element) {
-	z.Set(a)
-	a.Set(b)
-	b.Set(z)
-}
 
 //TODO: Very not constant time
 func (z *Element) SetInt64(i int64) {
-	var neg = i < 0
-	var abs uint64
-	if neg {
-		abs = uint64(-i)
-	} else {
-		abs = uint64(i)
-	}
-
-	z.SetUint64(abs)
-	z.Neg(z)
+	z.MulWord(&rSquare, i)
 }
 
 //LtAsIs compares without changing back from Montgomery form
@@ -58,27 +24,8 @@ func (z *Element) LtAsIs(x *Element) bool {
 	return z[3] < x[3]
 }
 
-func InverseIterate(a *Element, b *Element, u *Element, v *Element) {
-
-	//TODO: The two branches don't take the same amount of time
-	if a.Bit(0) == 0 {
-		a.Halve()
-		u.Halve()
-	} else {
-		if a.LtAsIs(b) {
-			*a, *b = *b, *a
-			*u, *v = *v, *a
-		}
-		//TODO: Exploit the shrinking of the lengths of a,b? Nah
-		a.Sub(a, b)
-		a.Halve()
-		u.Sub(u, v)
-		u.Halve()
-	}
-}
-
 //z is known in the algorithm as u. Renamed for receiver name consistency
-func (z *Element) Inverse(x *Element) *Element {
+func (z *Element) Inverse0(x *Element) *Element {
 
 	if x.IsZero() {
 		z.SetZero()
@@ -92,7 +39,7 @@ func (z *Element) Inverse(x *Element) *Element {
 
 	a = *x
 	b = qElement
-	u.SetOne() //TODO: Don't use u as working variable
+	u.SetOne()
 	v.SetZero()
 
 	//Loop bound according to P20 pg 3 footnote 2 (x is known to be invertible)
@@ -118,78 +65,77 @@ func (z *Element) Inverse(x *Element) *Element {
 	return z
 }
 
-var pornin1CorrectiveFactor = Element{
+/*var pornin1CorrectiveFactor = Element{
 	6477271515855170771,
 	5316309407648344051,
 	16881476412543395616,
 	2198450133018044985,
-}
+}*/
 
-func (z *Element) InverseOptimization1(x *Element) *Element {
+var pornin1CorrectiveFactor = Element{64, 0, 0, 0} //Not sure why the value is so clean, but I think I like it
+
+func (z *Element) Inverse(x *Element) *Element {
+
 	if x.IsZero() {
 		z.SetZero()
 		return z
 	}
 
-	var a big.Int
-	var b big.Int
+	var a Element
+	var b Element
+	var u Element
 	var v Element
-	var bigIntScratch big.Int //Copy values when
-	var elementScratch Element
-	var intScratch int64
 
 	//Update factors: we get [u; v]:= [f0 g0; f1 g1] [u; v]
 	//TODO: Better or worse to group two of them in the same var? Paper suggests it's used in "most optimized" implementation but algorithm 2 doesn't use it
-	var f0 int64
-	var g0 int64
-	var f1 int64
-	var g1 int64
+	var f0 int64 = 1
+	var g0 int64 = 0
+	var f1 int64 = 0
+	var g1 int64 = 1
 
-	x.ToBigInt(&a)
-	b.Set(Modulus())
-	z.SetOne()
+	a = *x
+	b = qElement
+	u.SetOne()
 	v.SetZero()
 
+	t := 1
 	//Loop bound according to P20 pg 3 footnote 2 (x is known to be invertible)
 	for iteration := 0; iteration < 2*Bits-2; iteration++ {
-		var t = iteration % 63
 		//TODO: The two branches don't take the same amount of time
 		if a.Bit(0) == 0 {
-			a.Rsh(&a, 1) //TODO: okay to use a itself?
+			a.Halve()
 			f1 *= 2
 			g1 *= 2
 		} else {
-			if a.Cmp(&b) < 0 {
-				swapBigInt(&a, &b, &bigIntScratch)
-				intScratch = f1
-				f1 = f0
-				f0 = intScratch
-				intScratch = g1
-				g1 = g0
-				g0 = intScratch
+			if a.LtAsIs(&b) {
+				a, b = b, a
+				f0, f1 = f1, f0
+				g0, g1 = g1, g0
 			}
-			//TODO: Exploit the shrinking of the lengths of a,b?
-			a.Sub(&a, &b) //TODO: This okay?
-			a.Rsh(&a, 1)  //TODO: ok?
+
+			a.Sub(&a, &b)
+			a.Halve()
 			f0 -= f1
 			g0 -= g1
 			f1 *= 2
 			g1 *= 2
 		}
 
-		if t == 63 {
+		if t == 62 || iteration == 2*Bits-3 { //TODO: 63 fails. Find out why
 			t = 0
+
+			var elementScratch Element
 			var updateFactor Element
 			//save u
-			elementScratch.Set(z)
+			elementScratch.Set(&u)
 
 			//update u
 			updateFactor.SetInt64(f0)
-			//TODO: Exploit the fact that the update factor is small?
-			z.Mul(z, &updateFactor)
+			u.Mul(&u, &updateFactor)
+
 			updateFactor.SetInt64(g0)
 			updateFactor.Mul(&v, &updateFactor)
-			z.Add(z, &updateFactor)
+			u.Add(&u, &updateFactor)
 
 			//update v
 			updateFactor.SetInt64(f1)
@@ -197,11 +143,14 @@ func (z *Element) InverseOptimization1(x *Element) *Element {
 			updateFactor.SetInt64(g1)
 			v.Mul(&v, &updateFactor)
 			v.Add(&v, &elementScratch)
+
+			f0, g0, f1, g1 = 1, 0, 0, 1
 		}
+		t++
 	}
 
-	//TODO: Multiply by rSquare x 2^(-2*Bits+2) instead
-	z.Mul(&v, &rSquare) //TODO: Would it have been okay to store it in v itself?
+	//z.Mul(&v, &pornin1CorrectiveFactor)
+	z.MulWord(&v, 64)
 	return z
 
 }
@@ -214,3 +163,112 @@ func (z *Element) InverseOptimization1(x *Element) *Element {
 
 
 }*/
+
+//TODO: Do this directly
+//TODO: If not done directly, the absolute value of y is less than half of uint64's capacity, could mean more carries are certainly zero
+func (z *Element) MulWord(x *Element, y int64) {
+	var neg bool
+	var abs uint64
+
+	if y < 0 {
+		neg = true
+		abs = uint64(-y)
+	} else {
+		neg = false
+		abs = uint64(y)
+	}
+
+	z.MulWordUnsigned(x, abs)
+
+	if neg {
+		z.Neg(z)
+	}
+}
+
+func (z *Element) MulWordUnsigned(x *Element, y uint64) {
+
+	var t [4]uint64
+	var c [3]uint64
+	{
+		// round 0
+		v := x[0]
+		c[1], c[0] = bits.Mul64(v, y)
+		m := c[0] * 9786893198990664585
+		c[2] = madd0(m, 4332616871279656263, c[0])
+
+		c[2], t[0] = madd2(m, 10917124144477883021, c[2], c[1])
+		c[2], t[1] = madd2(m, 13281191951274694749, c[2], 0)
+		t[3], t[2] = madd1(m, 3486998266802970665, c[2])
+
+		/*if t[0] != 8372381935743341968 {
+			panic("Oopsy poopsy")
+		}*/
+	}
+	{
+		// round 1
+		v := x[1]
+		c[1], c[0] = madd1(v, y, t[0])
+		m := c[0] * 9786893198990664585
+		c[2] = madd0(m, 4332616871279656263, c[0])
+
+		//c[1], c[0] = madd2(v, y[1], c[1], t[1])
+		c[0], c[1] = bits.Add64(c[1], t[1], 0)
+		c[2], t[0] = madd2(m, 10917124144477883021, c[2], c[0])
+
+		//c[1], c[0] = madd2(v, y[2], c[1], t[2])
+		c[0], c[1] = bits.Add64(c[1], t[2], 0)
+		c[2], t[1] = madd2(m, 13281191951274694749, c[2], c[0])
+
+		//c[1], c[0] = madd2(v, y[3], c[1], t[3])
+		c[0], c[1] = bits.Add64(c[1], t[3], 0)
+		t[3], t[2] = madd3(m, 3486998266802970665, c[0], c[2], c[1])
+	}
+	{
+		// round 2
+		v := x[2]
+		c[1], c[0] = madd1(v, y, t[0])
+		m := c[0] * 9786893198990664585
+		c[2] = madd0(m, 4332616871279656263, c[0])
+
+		//c[1], c[0] = madd2(v, y[1], c[1], t[1])
+		c[0], c[1] = bits.Add64(c[1], t[1], 0)
+		c[2], t[0] = madd2(m, 10917124144477883021, c[2], c[0])
+
+		//c[1], c[0] = madd2(v, y[2], c[1], t[2])
+		c[0], c[1] = bits.Add64(c[1], t[2], 0)
+		c[2], t[1] = madd2(m, 13281191951274694749, c[2], c[0])
+
+		//c[1], c[0] = madd2(v, y[3], c[1], t[3])
+		c[0], c[1] = bits.Add64(c[1], t[3], 0)
+		t[3], t[2] = madd3(m, 3486998266802970665, c[0], c[2], c[1])
+	}
+	{
+		// round 3
+		v := x[3]
+		c[1], c[0] = madd1(v, y, t[0])
+		m := c[0] * 9786893198990664585
+		c[2] = madd0(m, 4332616871279656263, c[0])
+
+		//c[1], c[0] = madd2(v, y[1], c[1], t[1])
+		c[0], c[1] = bits.Add64(c[1], t[1], 0)
+		c[2], z[0] = madd2(m, 10917124144477883021, c[2], c[0])
+
+		//c[1], c[0] = madd2(v, y[2], c[1], t[2])
+		c[0], c[1] = bits.Add64(c[1], t[2], 0)
+		c[2], z[1] = madd2(m, 13281191951274694749, c[2], c[0])
+
+		//c[1], c[0] = madd2(v, y[3], c[1], t[3])
+		c[0], c[1] = bits.Add64(c[1], t[3], 0)
+		z[3], z[2] = madd3(m, 3486998266802970665, c[0], c[2], c[1])
+	}
+
+	// if z > q --> z -= q
+	// note: this is NOT constant time
+	if !(z[3] < 3486998266802970665 || (z[3] == 3486998266802970665 && (z[2] < 13281191951274694749 || (z[2] == 13281191951274694749 && (z[1] < 10917124144477883021 || (z[1] == 10917124144477883021 && (z[0] < 4332616871279656263))))))) {
+		var b uint64
+		z[0], b = bits.Sub64(z[0], 4332616871279656263, 0)
+		z[1], b = bits.Sub64(z[1], 10917124144477883021, b)
+		z[2], b = bits.Sub64(z[2], 13281191951274694749, b)
+		z[3], _ = bits.Sub64(z[3], 3486998266802970665, b)
+	}
+}
