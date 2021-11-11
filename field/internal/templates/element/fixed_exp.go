6d5c7db34b9defc17193392a0efbe758ca59f345
Gautam Botrel <gautam.botrel@gmail.com>
2021-11-11 08:31:13 -0600

    fix: fixed exp template takes element name as parameter

--- field/internal/templates/element/fixed_exp.go
+++ field/internal/templates/element/fixed_exp.go
@@ -3,22 +3,22 @@ package element
 const FixedExp = `
 
 {{- if .SqrtQ3Mod4}}
-	{{expByAddChain "SqrtExp" .SqrtQ3Mod4ExponentData}}
+	{{expByAddChain "SqrtExp" .SqrtQ3Mod4ExponentData .ElementName}}
 {{- else if .SqrtAtkin}}
-	{{expByAddChain "SqrtExp" .SqrtAtkinExponentData}}
+	{{expByAddChain "SqrtExp" .SqrtAtkinExponentData .ElementName}}
 {{- else if .SqrtTonelliShanks}}
-	{{expByAddChain "SqrtExp" .SqrtSMinusOneOver2Data}}
+	{{expByAddChain "SqrtExp" .SqrtSMinusOneOver2Data .ElementName}}
 {{- end }}
 
-{{expByAddChain "LegendreExp" .LegendreExponentData}}
+{{expByAddChain "LegendreExp" .LegendreExponentData .ElementName}}
 
 
-{{define "expByAddChain name data"}}
+{{define "expByAddChain name data eName"}}
 	
 // expBy{{.name}} is equivalent to z.Exp(x, {{ .data.N }})
 // 
 // uses {{ .data.Meta.Module }} {{ .data.Meta.ReleaseTag }} to generate a shorter addition chain
-func (z *Element) expBy{{.name}}(x *Element) *Element {
+func (z *{{.eName}}) expBy{{$.name}}(x *{{.eName}}) *{{.eName}} {
 	// addition chain:
 	//
 	{{- range lines_ (format_ .data.Script) }}
@@ -28,7 +28,7 @@ func (z *Element) expBy{{.name}}(x *Element) *Element {
 	// Operations: {{ .data.Ops.Doubles }} squares {{ .data.Ops.Adds }} multiplies
 
 	// Allocate Temporaries.
-	var {{range $i, $e := .data.Program.Temporaries }}{{ $e }} {{- if last_ $i $.data.Program.Temporaries}} Element {{- else }}, {{- end}}{{- end -}}
+	var {{range $i, $e := .data.Program.Temporaries }}{{ $e }} {{- if last_ $i $.data.Program.Temporaries}} {{$.eName}} {{- else }}, {{- end}}{{- end -}}
 
 	{{ range $i := .data.Program.Instructions }}
 	// {{ printf "Step %d: %s = x^%#x" $i.Output.Index $i.Output (index $.data.Chain $i.Output.Index) }}
