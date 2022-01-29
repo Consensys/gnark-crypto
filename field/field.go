4abd08306830c70085a7fa2cf727ff354bcfe056
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2022-01-28 20:05:17 -0500

    test: obnoxious verbosity

--- field/field.go
+++ field/field.go
@@ -349,6 +349,7 @@ func BigIntMatchUint64Slice(aInt *big.Int, a []uint64) error {
 	words := aInt.Bits()
 
 	const steps = 64 / bits.UintSize
+	const filter uint64 = 0xFFFFFFFFFFFFFFFF >> (64 - bits.UintSize)
 	for i := 0; i < len(a)*steps; i++ {
 
 		var wI big.Word
@@ -357,10 +358,10 @@ func BigIntMatchUint64Slice(aInt *big.Int, a []uint64) error {
 			wI = words[i]
 		}
 
-		const filter uint64 = 0xFFFFFFFFFFFFFFFF >> (64 - bits.UintSize)
-
 		aI := a[i] >> ((i * bits.UintSize) % 64)
 		aI &= filter
+
+		fmt.Printf("at %d: orig=%x, ints=%x\n", i, wI, aI)
 
 		if uint64(wI) != aI {
 			return fmt.Errorf("bignum mismatch: disagreement on word %d: %x ≠ %x; %d ≠ %d", i, uint64(wI), aI, uint64(wI), aI)
