6ffa3c75b5f33a18735ec9825b34edc1e5450a19
Youssef El Housni <youssef.housni21@gmail.com>
2022-02-10 12:37:01 +0100

    fix(tEdwards): cofactor ToMont() not FromMont()

--- ecc/bls12-377/twistededwards/twistededwards.go
+++ ecc/bls12-377/twistededwards/twistededwards.go
@@ -51,7 +51,7 @@ func init() {
 
 	edwards.A.SetOne().Neg(&edwards.A)
 	edwards.D.SetUint64(3021)
-	edwards.Cofactor.SetUint64(4).FromMont()
+	edwards.Cofactor.SetUint64(4).ToMont()
 	edwards.Order.SetString("2111115437357092606062206234695386632838870926408408195193685246394721360383", 10)
 
 	edwards.Base.X.SetString("717051916204163000937139483451426116831771857428389560441264442629694842243")
