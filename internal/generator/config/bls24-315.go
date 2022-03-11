4957a576aff7bc3889cbe3f7565a838cb14e41a5
Youssef El Housni <youssef.housni21@gmail.com>
2022-03-11 10:50:05 +0100

    feat: sweet parameters for BLS24-315 G1 SSWU

--- internal/generator/config/bls24-315.go
+++ internal/generator/config/bls24-315.go
@@ -23,6 +23,36 @@ var BLS24_315 = Curve{
 		CRange:           defaultCRange(),
 		Projective:       true,
 	},
+	HashE1: &HashSuite{
+		A: []string{"0x4c23a0197b9ca68541a4cef14af4cfe81cc324cac5626d9ff4ee66df9ea2678877910f40300001f"},
+		B: []string{"0x16"},
+		Z: []int{13},
+		Isogeny: &Isogeny{
+			XMap: RationalPolynomial{
+				Num: [][]string{
+					{"0x2611d014c792a8ffd30982483b3ee757787d35c9e880e096a850c8e24edf5c71f880eff103c0002"},
+					{"0x2611d01644a40e35d2dad31956fafeee9f1a0831db5b7b49ac10c81d6ff9afd483bf88000000000"},
+					{"0x391ab82082520bc9ef97728ef1d4703e7115c13f9db942831972c63be0e6bc1d3ee023f70240001"},
+				},
+				Den: [][]string{
+					{"0x261b56ebccc821ae82c6025bea42e1d731e2a911e6c66652b682a1f0411fdc017f9ffffe"},
+				},
+			},
+			YMap: RationalPolynomial{
+				Num: [][]string{
+					{"0x391ab82082520bc9ef97728ef1d4703e7115c13f9db942831972c63be0e6bc1d3ee023f7023ffff"},
+					{"0x391ab822bdec239aef516bc89b6e93a12b00fcdb8a012a8f9f12c514928e39310fbe080d7c9fffd"},
+					{"0x391ab82166f61550bc483ca602787e65eea70c4ac90938ee82192c2c27f687bec59f4c000000000"},
+					{"0x429f2c25ed5fb86b978605a6c4cd2d9e2e996174e2ad78439db091f0866286221eb029f582a0001"},
+				},
+				Den: [][]string{
+					{"0x4c23a02b586d650d3f7498be97c5eafdec1d01aa27a1ae0421ee5da52bde5026fe802ff402ffff9"},
+					{"0xe4a40986ccb0ca1710a40e277d914b0b2b4ff66b68a665f0470fcba186bf2808fdbfffe8"},
+					{"0x725204c36658650b88520713bec8a58595a7fb35b45332f82387e5d0c35f94047edffffa"},
+				},
+			},
+		},
+	},
 }
 
 func init() {
