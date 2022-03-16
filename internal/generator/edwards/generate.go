22fa11e802ea44d7c4c0b4cfc167f0d85c371355
Gautam Botrel <gautam.botrel@gmail.com>
2022-03-16 13:38:25 -0500

    refactor: twistededwards.go -> curve.go

--- internal/generator/edwards/generate.go
+++ internal/generator/edwards/generate.go
@@ -8,14 +8,12 @@ import (
 )
 
 func Generate(conf config.TwistedEdwardsCurve, baseDir string, bgen *bavard.BatchGenerator) error {
-
 	entries := []bavard.Entry{
 		{File: filepath.Join(baseDir, "point.go"), Templates: []string{"point.go.tmpl"}},
 		{File: filepath.Join(baseDir, "point_test.go"), Templates: []string{"tests/point.go.tmpl"}},
 		{File: filepath.Join(baseDir, "doc.go"), Templates: []string{"doc.go.tmpl"}},
-		{File: filepath.Join(baseDir, "twistededwards.go"), Templates: []string{"curve.go.tmpl"}},
+		{File: filepath.Join(baseDir, "curve.go"), Templates: []string{"curve.go.tmpl"}},
 	}
 
 	return bgen.Generate(conf, conf.Package, "./edwards/template", entries...)
-
 }
