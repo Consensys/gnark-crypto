39aa1a463a2d606a02aad9ee6973f0a32df732c0
Gautam Botrel <gautam.botrel@gmail.com>
2022-04-25 10:32:38 -0500

    feat: add (unoptimized) goldilock field

---
+++ field/goldilocks/internal/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/consensys/gnark-crypto/field"
+	"github.com/consensys/gnark-crypto/field/generator"
+)
+
+//go:generate go run main.go
+func main() {
+	const modulus = "0xFFFFFFFF00000001"
+	goldilocks, err := field.NewField("integration", "Element", modulus, false)
+	if err != nil {
+		panic(err)
+	}
+	if err := generator.GenerateFF(goldilocks, "../"); err != nil {
+		panic(err)
+	}
+	fmt.Println("successfully generated goldilocks field")
+}
