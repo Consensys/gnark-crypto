f63a4dbad055a234281f5af1441d10cb049a0940
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2022-05-17 16:46:33 -0400

    refactor: prefer SSWU to SvdW

--- ecc/bls12-378/hash_to_curve.go
+++ ecc/bls12-378/hash_to_curve.go
@@ -17,31 +17,9 @@ package bls12378
 import (
 	"math/big"
 
-	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark-crypto/ecc/bls12-378/fp"
 	"github.com/consensys/gnark-crypto/ecc/bls12-378/internal/fptower"
 )
-
-// hashToFp hashes msg to count prime field elements.
-// https://tools.ietf.org/html/draft-irtf-cfrg-hash-to-curve-06#section-5.2
-func hashToFp(msg, dst []byte, count int) ([]fp.Element, error) {
-
-	// 128 bits of security
-	// L = ceil((ceil(log2(p)) + k) / 8), where k is the security parameter = 128
-	L := 64
-
-	lenInBytes := count * L
-	pseudoRandomBytes, err := ecc.ExpandMsgXmd(msg, dst, lenInBytes)
-	if err != nil {
-		return nil, err
-	}
-
-	res := make([]fp.Element, count)
-	for i := 0; i < count; i++ {
-		res[i].SetBytes(pseudoRandomBytes[i*L : (i+1)*L])
-	}
-	return res, nil
-}
 
 // returns false if u>-u when seen as a bigInt
 func sign0(u fp.Element) bool {
@@ -50,111 +28,6 @@ func sign0(u fp.Element) bool {
 	u.Neg(&u)
 	u.ToBigIntRegular(&b)
 	return a.Cmp(&b) <= 0
-}
-
-// ----------------------------------------------------------------------------------------
-// G1Affine
-
-// https://tools.ietf.org/html/draft-irtf-cfrg-hash-to-curve-06#section-4.1
-// Shallue and van de Woestijne method, works for any elliptic curve in Weierstrass curve
-func svdwMapG1(u fp.Element) G1Affine {
-
-	var res G1Affine
-
-	// constants
-	// sage script to find z: https://tools.ietf.org/html/draft-irtf-cfrg-hash-to-curve-06#appendix-E.1
-	var z, c1, c2, c3, c4 fp.Element
-	z.SetOne()
-	c1.SetString("2")
-	c2.SetString("302624103037653085866624240790900480369923845885462456876760372017370467951700652388141901174418655585487141470208")
-	c3.SetString("287149757441151686413597772668332250774959033155223733277500818890482204710560507876289457345990628536787382328589")
-	c4.SetString("403498804050204114488832321054533973826565127847283275835680496023160623935600869850855868232558207447316188626942")
-
-	var tv1, tv2, tv3, tv4, one, x1, gx1, x2, gx2, x3, x, gx, y fp.Element
-	one.SetOne()
-	tv1.Square(&u).Mul(&tv1, &c1)
-	tv2.Add(&one, &tv1)
-	tv1.Sub(&one, &tv1)
-	tv3.Mul(&tv2, &tv1).Inverse(&tv3)
-	tv4.Mul(&u, &tv1)
-	tv4.Mul(&tv4, &tv3)
-	tv4.Mul(&tv4, &c3)
-	x1.Sub(&c2, &tv4)
-	gx1.Square(&x1)
-	// 12. gx1 = gx1 + A
-	gx1.Mul(&gx1, &x1)
-	gx1.Add(&gx1, &bCurveCoeff)
-	e1 := gx1.Legendre()
-	x2.Add(&c2, &tv4)
-	gx2.Square(&x2)
-	// 18. gx2 = gx2 + A
-	gx2.Mul(&gx2, &x2)
-	gx2.Add(&gx2, &bCurveCoeff)
-	e2 := gx2.Legendre() - e1 // 2 if is_square(gx2) AND NOT e1
-	x3.Square(&tv2)
-	x3.Mul(&x3, &tv3)
-	x3.Square(&x3)
-	x3.Mul(&x3, &c4)
-	x3.Add(&x3, &z)
-	if e1 == 1 {
-		x.Set(&x1)
-	} else {
-		x.Set(&x3)
-	}
-	if e2 == 2 {
-		x.Set(&x2)
-	}
-	gx.Square(&x)
-	// gx = gx + A
-	gx.Mul(&gx, &x)
-	gx.Add(&gx, &bCurveCoeff)
-	y.Sqrt(&gx)
-	e3 := sign0(u) && sign0(y)
-	if !e3 {
-		y.Neg(&y)
-	}
-	res.X.Set(&x)
-	res.Y.Set(&y)
-
-	return res
-}
-
-// MapToCurveG1Svdw maps an fp.Element to a point on the curve using the Shallue and van de Woestijne map
-// https://tools.ietf.org/html/draft-irtf-cfrg-hash-to-curve-06#section-2.2.1
-func MapToCurveG1Svdw(t fp.Element) G1Affine {
-	res := svdwMapG1(t)
-	res.ClearCofactor(&res)
-	return res
-}
-
-// EncodeToCurveG1Svdw maps an fp.Element to a point on the curve using the Shallue and van de Woestijne map
-// https://tools.ietf.org/html/draft-irtf-cfrg-hash-to-curve-06#section-2.2.2
-func EncodeToCurveG1Svdw(msg, dst []byte) (G1Affine, error) {
-	var res G1Affine
-	t, err := hashToFp(msg, dst, 1)
-	if err != nil {
-		return res, err
-	}
-	res = MapToCurveG1Svdw(t[0])
-	return res, nil
-}
-
-// HashToCurveG1Svdw maps an fp.Element to a point on the curve using the Shallue and van de Woestijne map
-// https://tools.ietf.org/html/draft-irtf-cfrg-hash-to-curve-06#section-3
-func HashToCurveG1Svdw(msg, dst []byte) (G1Affine, error) {
-	var res G1Affine
-	u, err := hashToFp(msg, dst, 2)
-	if err != nil {
-		return res, err
-	}
-	Q0 := MapToCurveG1Svdw(u[0])
-	Q1 := MapToCurveG1Svdw(u[1])
-	var _Q0, _Q1, _res G1Jac
-	_Q0.FromAffine(&Q0)
-	_Q1.FromAffine(&Q1)
-	_res.Set(&_Q1).AddAssign(&_Q0)
-	res.FromJacobian(&_res)
-	return res, nil
 }
 
 // ----------------------------------------------------------------------------------------
