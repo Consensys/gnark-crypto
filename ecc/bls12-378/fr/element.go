ebaac167d51f4df97e448a4bc81500307b454b8d
Gautam Botrel <gautam.botrel@gmail.com>
2022-05-24 22:06:37 -0500

    fix: remove dead code

--- ecc/bls12-378/fr/element.go
+++ ecc/bls12-378/fr/element.go
@@ -20,7 +20,6 @@ import (
 	"crypto/rand"
 	"encoding/binary"
 	"errors"
-	"io"
 	"math/big"
 	"math/bits"
 	"reflect"
@@ -315,45 +314,6 @@ func (z *Element) LexicographicallyLargest() bool {
 	_, b = bits.Sub64(_z[3], 1185534000748140376, b)
 
 	return b == 0
-}
-
-func Int(rand io.Reader, max *big.Int) (n *big.Int, err error) {
-	if max.Sign() <= 0 {
-		panic("crypto/rand: argument to Int is <= 0")
-	}
-	n = new(big.Int)
-	n.Sub(max, n.SetUint64(1))
-	// bitLen is the maximum bit length needed to encode a value < max.
-	bitLen := n.BitLen()
-	if bitLen == 0 {
-		// the only valid result is 0
-		return
-	}
-	// k is the maximum byte length needed to encode a value < max.
-	k := (bitLen + 7) / 8
-	// b is the number of bits in the most significant byte of max-1.
-	b := uint(bitLen % 8)
-	if b == 0 {
-		b = 8
-	}
-
-	bytes := make([]byte, k)
-
-	for {
-		_, err = io.ReadFull(rand, bytes)
-		if err != nil {
-			return nil, err
-		}
-
-		// Clear bits in the first byte to increase the probability
-		// that the candidate is < max.
-		bytes[0] &= uint8(int(1<<b) - 1)
-
-		n.SetBytes(bytes)
-		if n.Cmp(max) < 0 {
-			return
-		}
-	}
 }
 
 // SetRandom sets z to a uniform random value in [0, q).
--- field/internal/templates/element/base.go
+++ field/internal/templates/element/base.go
@@ -7,7 +7,6 @@ import (
 	"math/bits"
 	"crypto/rand"
 	"encoding/binary"
-	"io"
 	"sync"
 	"strconv"
 	"errors"
@@ -299,46 +298,6 @@ func (z *{{.ElementName}}) LexicographicallyLargest() bool {
 	{{- end}}
 
 	return b == 0
-}
-
-
-func Int(rand io.Reader, max *big.Int) (n *big.Int, err error) {
-	if max.Sign() <= 0 {
-		panic("crypto/rand: argument to Int is <= 0")
-	}
-	n = new(big.Int)
-	n.Sub(max, n.SetUint64(1))
-	// bitLen is the maximum bit length needed to encode a value < max.
-	bitLen := n.BitLen()
-	if bitLen == 0 {
-		// the only valid result is 0
-		return
-	}
-	// k is the maximum byte length needed to encode a value < max.
-	k := (bitLen + 7) / 8
-	// b is the number of bits in the most significant byte of max-1.
-	b := uint(bitLen % 8)
-	if b == 0 {
-		b = 8
-	}
-
-	bytes := make([]byte, k)
-
-	for {
-		_, err = io.ReadFull(rand, bytes)
-		if err != nil {
-			return nil, err
-		}
-
-		// Clear bits in the first byte to increase the probability
-		// that the candidate is < max.
-		bytes[0] &= uint8(int(1<<b) - 1)
-
-		n.SetBytes(bytes)
-		if n.Cmp(max) < 0 {
-			return
-		}
-	}
 }
 
 // SetRandom sets z to a uniform random value in [0, q).
