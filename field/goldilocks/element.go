d54dfc14eb14ef19a680a5e3c10f0e5c1e576ce4
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2022-08-17 14:17:23 -0400

    perf: add branch-carry perf and some docs

--- field/goldilocks/element.go
+++ field/goldilocks/element.go
@@ -394,12 +394,27 @@ func (z *Element) Mul(x, y *Element) *Element {
 	// → t is a temporary array of size N+2
 	// → C, S are machine words. A pair (C,S) refers to (hi-bits, lo-bits) of a two-word number
 
+	// In fact, since the modulus R fits on one register, the CIOS algorithm gets reduced to standard REDC (textbook Montgomery reduction):
+	// hi, lo := x * y
+	// m := (lo * qInvNeg) mod R
+	// (*) r := (hi * R + lo + m * q) / R
+	// reduce r if necessary
+
+	// On the emphasized line, we get r = hi + (lo + m * q) / R
+	// If we write hi2, lo2 = m * q then lo2 + R hi2 = m * q ⇒ R | m * q - lo2 ⇒ R | (lo * qInvNeg) q - lo2 = -lo - lo2
+	// This shows lo + lo2 = 0 mod R. i.e. lo + lo2 = 0 if lo = 0 and R otherwise.
+	// Which finally gives (lo + m * q) / R = (lo + lo2 + R hi2) / R = hi2 + (lo+lo2) / R = hi2 + (lo != 0)
+	// This "optimization" lets us do away with one MUL instruction on ARM architectures and is available for all q < R.
+
 	var r uint64
 	hi, lo := bits.Mul64(x[0], y[0])
+	if lo != 0 {
+		hi++ // x[0] * y[0] ≤ 2¹²⁸ - 2⁶⁵ + 1, meaning hi ≤ 2⁶⁴ - 2 so no need to worry about overflow
+	}
+
 	m := lo * qInvNeg
 	hi2, _ := bits.Mul64(m, q)
-	_, carry := bits.Add64(lo, 0xffffffffffffffff, 0)
-	r, carry = bits.Add64(hi2, hi, carry)
+	r, carry := bits.Add64(hi2, hi, 0)
 
 	if carry != 0 || r >= q {
 		// we need to reduce
