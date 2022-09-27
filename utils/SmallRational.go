eb13095f53df01b601260293724b36fb8df49b33
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2022-09-27 17:21:42 -0400

    feat: neg, sub, equal, double

--- utils/SmallRational.go
+++ utils/SmallRational.go
@@ -1,5 +1,7 @@
 package utils
 
+import "math/bits"
+
 type SmallRational struct {
 	numerator   int64
 	denominator int64
@@ -26,5 +28,66 @@ func (z *SmallRational) Inverse(x *SmallRational) *SmallRational {
 	} else {
 		*z = SmallRational{x.denominator, x.numerator}
 	}
+	return z
+}
+
+func (z *SmallRational) Neg(x *SmallRational) *SmallRational {
+	*z = SmallRational{-x.numerator, x.denominator}
+	return z
+}
+
+func (z *SmallRational) Double(x *SmallRational) *SmallRational {
+	if x.denominator%2 == 0 {
+		*z = SmallRational{x.numerator, x.denominator / 2}
+	} else {
+		*z = SmallRational{x.numerator * 2, x.denominator / 2}
+	}
+	return z
+}
+
+func (z *SmallRational) sign() int {
+	if z.IsZero() {
+		return 0
+	}
+	if z.numerator > 0 {
+		if z.denominator > 0 {
+			return 1
+		}
+		return -1
+	}
+	if z.denominator > 0 {
+		return -1
+	}
+	return 1
+}
+
+func (z *SmallRational) abs() (abs SmallRational) {
+	abs = *z
+	if abs.numerator < 0 {
+		abs.numerator = -abs.numerator
+	}
+	if abs.denominator < 0 {
+		abs.denominator = -abs.denominator
+	}
+	return abs
+}
+
+func (z *SmallRational) Equal(x *SmallRational) bool {
+	zSign := z.sign()
+	if zSign != x.sign() {
+		return false
+	}
+	xAbs, zAbs := x.abs(), z.abs()
+
+	cross0Hi, cross0Lo := bits.Mul64(uint64(xAbs.numerator), uint64(zAbs.denominator))
+	cross1Hi, cross1Lo := bits.Mul64(uint64(zAbs.numerator), uint64(xAbs.denominator))
+
+	return cross1Hi == cross0Hi && cross1Lo == cross0Lo
+}
+
+func (z *SmallRational) Sub(x, y *SmallRational) *SmallRational {
+	var yNeg SmallRational
+	yNeg.Neg(y)
+	z.Add(x, &yNeg)
 	return z
 }
