6cd5c4eb5648b3bd82477924954acc7cac5ddd27
zhiqiangxu <652732310@qq.com>
2022-11-16 00:41:48 +0800

    add Decompose

---
+++ utils/decompose.go
@@ -0,0 +1,21 @@
+package utils
+
+import "math/big"
+
+func Decompose(rawBytes []byte, modulos *big.Int) (decomposed []byte) {
+	raw := big.NewInt(0).SetBytes(rawBytes)
+
+	var chunk [32]byte
+	decomposed = make([]byte, 0, len(rawBytes))
+	for raw.Cmp(modulos) >= 0 {
+		mod := big.NewInt(0).Mod(raw, modulos)
+		mod.FillBytes(chunk[:])
+		decomposed = append(decomposed, chunk[:]...)
+
+		raw.Div(raw, modulos)
+	}
+
+	raw.FillBytes(chunk[:])
+	decomposed = append(decomposed, chunk[:]...)
+	return decomposed
+}
