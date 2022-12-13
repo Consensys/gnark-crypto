dd27bf383d4b46f90c8df0ef65698f083a41ea4d
Thomas Piellard <thomas.piellard@consensys.net>
2022-12-13 16:12:44 +0100

    fix: fixed templates

--- internal/generator/iop/generate.go
+++ internal/generator/iop/generate.go
@@ -13,7 +13,7 @@ func Generate(conf config.Curve, baseDir string, bgen *bavard.BatchGenerator) er
 	conf.Package = "iop"
 	entries := []bavard.Entry{
 		{File: filepath.Join(baseDir, "doc.go"), Templates: []string{"doc.go.tmpl"}},
-		{File: filepath.Join(baseDir, "polynomial.go"), Templates: []string{"polyonomial.go.tmpl"}},
+		{File: filepath.Join(baseDir, "polynomial.go"), Templates: []string{"polynomial.go.tmpl"}},
 		{File: filepath.Join(baseDir, "polynomial_test.go"), Templates: []string{"polynomial.test.go.tmpl"}},
 
 		{File: filepath.Join(baseDir, "ratios.go"), Templates: []string{"ratios.go.tmpl"}},
@@ -25,6 +25,6 @@ func Generate(conf config.Curve, baseDir string, bgen *bavard.BatchGenerator) er
 		{File: filepath.Join(baseDir, "utils.go"), Templates: []string{"utils.go.tmpl"}},
 		{File: filepath.Join(baseDir, "utils_test.go"), Templates: []string{"utils.test.go.tmpl"}},
 	}
-	return bgen.Generate(conf, conf.Package, "./template/", entries...)
+	return bgen.Generate(conf, conf.Package, "./iop/template/", entries...)
 
 }
