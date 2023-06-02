0901a2430d0bf629a2145385702665421a0b63be
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2023-06-02 12:25:26 -0500

    feat: pedersen(bn254).WriteRawTo

--- utils/testing.go
+++ utils/testing.go
@@ -13,11 +13,32 @@ type Serializable interface {
 	io.WriterTo
 }
 
+type RawSerializable interface {
+	WriteRawTo(io.Writer) (int64, error)
+}
+
 func SerializationRoundTrip(o Serializable) func(*testing.T) {
 	return func(t *testing.T) {
 		// serialize it...
 		var buf bytes.Buffer
 		_, err := o.WriteTo(&buf)
+		assert.NoError(t, err)
+
+		// reconstruct the object
+		_o := reflect.New(reflect.TypeOf(o).Elem()).Interface().(Serializable)
+		_, err = _o.ReadFrom(&buf)
+		assert.NoError(t, err)
+
+		// compare
+		assert.Equal(t, o, _o)
+	}
+}
+
+func SerializationRoundTripRaw(o RawSerializable) func(*testing.T) {
+	return func(t *testing.T) {
+		// serialize it...
+		var buf bytes.Buffer
+		_, err := o.WriteRawTo(&buf)
 		assert.NoError(t, err)
 
 		// reconstruct the object
