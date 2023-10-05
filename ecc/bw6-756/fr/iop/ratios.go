8144fd72563aa7d418da7d6a2c9f5669c4c66e35
Gautam Botrel <gautam.botrel@gmail.com>
2023-10-05 13:35:22 -0500

    fix: handle edge case where provided domain cardinality is 1

--- ecc/bw6-756/fr/iop/ratios.go
+++ ecc/bw6-756/fr/iop/ratios.go
@@ -329,16 +329,30 @@ func buildDomain(n int, domain *fft.Domain) (*fft.Domain, error) {
 // nbCopies is the number of cosets of the roots of unity that are needed, including the set of
 // roots of unity itself.
 func getSupportIdentityPermutation(nbCopies int, domain *fft.Domain) []fr.Element {
+	if nbCopies <= 0 {
+		panic("getSupportIdentityPermutation: nbCopies must be positive")
+	}
 
 	res := make([]fr.Element, uint64(nbCopies)*domain.Cardinality)
 	sizePoly := int(domain.Cardinality)
 
-	// len(domain.Twiddle) == sizePoly / 2
-	copy(res, domain.Twiddles[0])
-	// remaining ones.
-	for i := (sizePoly / 2) - 1; i < sizePoly-1; i++ {
-		res[i+1].Mul(&res[i], &domain.Generator)
+	// check if we can reuse the pre-computed twiddles from the domain.
+	if len(domain.Twiddles) == 0 || len(domain.Twiddles[0]) < sizePoly/2 {
+		res[0].SetOne()
+		if len(res) > 1 {
+			res[1].Set(&domain.Generator)
+			for i := 2; i < len(res); i++ {
+				res[i].Mul(&res[i-1], &domain.Generator)
+			}
+		}
+	} else {
+		// re-use pre-computed twiddles from the domain.
+		copy(res, domain.Twiddles[0])
+		for i := (sizePoly / 2) - 1; i < sizePoly-1; i++ {
+			res[i+1].Mul(&res[i], &domain.Generator)
+		}
 	}
+
 	if nbCopies <= 1 {
 		return res
 	}
