4fc5102c0cd92ce124c6b2ddcb57ed6e34324e24
Gautam Botrel <gautam.botrel@gmail.com>
2023-10-12 13:38:36 -0500

    test: add a failing test to try CI output

--- ecc/utils_test.go
+++ ecc/utils_test.go
@@ -15,7 +15,7 @@ func TestNafDecomposition(t *testing.T) {
 
 	res := [5]int8{1, 0, -1, 0, 1}
 	for i, v := range dec {
-		if v != res[i] {
+		if v == res[i] {
 			t.Error("Error in NafDecomposition")
 		}
 	}
