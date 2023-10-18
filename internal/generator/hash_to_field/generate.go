c739ecad54d4cae252f148b7041b8d50a6967bc4
Ivo Kubjas <ivo.kubjas@consensys.net>
2023-10-18 20:13:04 +0200

    test: add consistency test

--- internal/generator/hash_to_field/generate.go
+++ internal/generator/hash_to_field/generate.go
@@ -11,6 +11,8 @@ func Generate(conf config.FieldDependency, baseDir string, bgen *bavard.BatchGen
 	entries := []bavard.Entry{
 		{File: filepath.Join(baseDir, "doc.go"), Templates: []string{"doc.go.tmpl"}},
 		{File: filepath.Join(baseDir, "hash_to_field.go"), Templates: []string{"hash_to_field.go.tmpl"}},
+		{File: filepath.Join(baseDir, "hash_to_field_test.go"), Templates: []string{"hash_to_field_test.go.tmpl"}},
 	}
+
 	return bgen.Generate(conf, "hash_to_field", "./hash_to_field/template", entries...)
 }
