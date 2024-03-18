8af301d90ee3c84340856798962312e68096791e
Thomas Piellard <thomas.piellard@consensys.net>
2024-03-18 18:37:43 +0100

    feat: add doc for fflonk

--- internal/generator/fflonk/generator.go
+++ internal/generator/fflonk/generator.go
@@ -12,11 +12,10 @@ func Generate(conf config.Curve, baseDir string, bgen *bavard.BatchGenerator) er
 	// kzg commitment scheme
 	conf.Package = "fflonk"
 	entries := []bavard.Entry{
-		// {File: filepath.Join(baseDir, "doc.go"), Templates: []string{"doc.go.tmpl"}},
+		{File: filepath.Join(baseDir, "doc.go"), Templates: []string{"doc.go.tmpl"}},
 		{File: filepath.Join(baseDir, "fflonk.go"), Templates: []string{"fflonk.go.tmpl"}},
 		{File: filepath.Join(baseDir, "fflonk_test.go"), Templates: []string{"fflonk.test.go.tmpl"}},
 		// {File: filepath.Join(baseDir, "marshal.go"), Templates: []string{"marshal.go.tmpl"}},
-		// {File: filepath.Join(baseDir, "utils.go"), Templates: []string{"utils.go.tmpl"}},
 	}
 	return bgen.Generate(conf, conf.Package, "./fflonk/template/", entries...)
 
