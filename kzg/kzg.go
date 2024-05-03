f9fac625954552196870b61a509eaee3f65bf6d0
Gautam Botrel <gautam.botrel@gmail.com>
2024-05-02 20:41:24 -0500

    fix: add BinaryDumper interface in kzg root package

--- kzg/kzg.go
+++ kzg/kzg.go
@@ -22,12 +22,15 @@ import (
 type Serializable interface {
 	io.ReaderFrom
 	io.WriterTo
+	BinaryDumper
 
 	WriteRawTo(w io.Writer) (n int64, err error)
 	UnsafeReadFrom(r io.Reader) (int64, error)
+}
 
-	UnsafeToBytes(maxPkPoints ...int) ([]byte, error)
-	UnsafeFromBytes(data []byte, maxPkPoints ...int) error
+type BinaryDumper interface {
+	WriteDump(w io.Writer, maxPkPoints ...int) error
+	ReadDump(r io.Reader, maxPkPoints ...int) error
 }
 
 type SRS Serializable
