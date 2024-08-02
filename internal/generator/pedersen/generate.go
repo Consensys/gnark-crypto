89e41ae288848b2bdb305db8d13eeb5f1c9749dd
Ivo Kubjas <ivo.kubjas@consensys.net>
2024-08-02 13:49:06 +0000

    docs: add documentation examples

--- internal/generator/pedersen/generate.go
+++ internal/generator/pedersen/generate.go
@@ -15,6 +15,7 @@ func Generate(conf config.Curve, baseDir string, bgen *bavard.BatchGenerator) er
 		{File: filepath.Join(baseDir, "doc.go"), Templates: []string{"doc.go.tmpl"}},
 		{File: filepath.Join(baseDir, "pedersen.go"), Templates: []string{"pedersen.go.tmpl"}},
 		{File: filepath.Join(baseDir, "pedersen_test.go"), Templates: []string{"pedersen.test.go.tmpl"}},
+		{File: filepath.Join(baseDir, "example_test.go"), Templates: []string{"example_test.go.tmpl"}},
 	}
 	return bgen.Generate(conf, conf.Package, "./pedersen/template/", entries...)
 
