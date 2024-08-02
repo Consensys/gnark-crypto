1a0fe1aff45e460559fc7bcaa1122fbd092b9405
Ivo Kubjas <ivo.kubjas@consensys.net>
2024-08-02 12:47:39 +0000

    docs: add package documentation

--- internal/generator/pedersen/generate.go
+++ internal/generator/pedersen/generate.go
@@ -12,6 +12,7 @@ func Generate(conf config.Curve, baseDir string, bgen *bavard.BatchGenerator) er
 	// pedersen commitment scheme
 	conf.Package = "pedersen"
 	entries := []bavard.Entry{
+		{File: filepath.Join(baseDir, "doc.go"), Templates: []string{"doc.go.tmpl"}},
 		{File: filepath.Join(baseDir, "pedersen.go"), Templates: []string{"pedersen.go.tmpl"}},
 		{File: filepath.Join(baseDir, "pedersen_test.go"), Templates: []string{"pedersen.test.go.tmpl"}},
 	}
