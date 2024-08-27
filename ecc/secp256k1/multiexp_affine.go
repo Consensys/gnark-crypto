ef903557e7b6f6c826ad85bd3973bae03ed3589c
Gautam Botrel <gautam.botrel@gmail.com>
2024-08-27 09:04:22 -0500

    perf: remove uneeded bucket init in affine coordinates

--- ecc/secp256k1/multiexp_affine.go
+++ ecc/secp256k1/multiexp_affine.go
@@ -59,10 +59,9 @@ func processChunkG1BatchAffine[BJE ibg1JacExtended, B ibG1Affine, BS bitSet, TP 
 	// note that we have 2 sets of buckets
 	// 1 in G1Affine used with the batch affine additions
 	// 1 in g1JacExtended used in case the queue of conflicting points
-	var buckets B
+	var buckets B // in G1Affine coordinates, infinities are represented as (0,0), no need to init
 	var bucketsJE BJE
 	for i := 0; i < len(buckets); i++ {
-		buckets[i].setInfinity()
 		bucketsJE[i].setInfinity()
 	}
 
