f6fb4da6e1e4c395ca4f68c24674f7e80374ffd3
Thomas Piellard <thomas.piellard@consensys.net>
2024-10-14 15:29:51 +0200

    feat: getters for the poseidon parameters

--- ecc/bw6-633/fr/poseidon2/poseidon2.go
+++ ecc/bw6-633/fr/poseidon2/poseidon2.go
@@ -71,6 +71,37 @@ func NewHash(t, d, rf, rp int) Hash {
 		params: parameters{t: t, d: d, rF: rf, rP: rp}}
 }
 
+// GetT returns the size of the state
+func (h *Hash) GetT() int {
+	return h.params.t
+}
+
+// GetRF returns the total number of full rounds (even number)
+func (h *Hash) GetRF() int {
+	return h.params.rF
+}
+
+// GetRP retursn the total number of partial rounds
+func (h *Hash) GetRP() int {
+	return h.params.rP
+}
+
+// GetD returns the degree of the sbox
+func (h *Hash) GetD() int {
+	return h.params.d
+}
+
+// GetInternalMatrix returns the diagonal elements of the internal matrix, minus one
+func (h *Hash) GetInternalMatrix() []fr.Element {
+	return h.params.diagInternalMatrices
+}
+
+// GetRC returns the round constants
+func (h *Hash) GetRC() [][]fr.Element {
+	return h.params.roundKeys
+}
+
+// Reset empty the state
 func (h *Hash) Reset() {
 	h.buffer = make([]fr.Element, 0, len(h.buffer))
 }
