9b32fcaa0222c067f03d1d795a4fdc18dd562482
Thomas Piellard <thomas.piellard@consensys.net>
2024-11-22 18:35:35 +0100

    feat: code gen

--- internal/generator/crypto/hash/poseidon2/generate.go
+++ internal/generator/crypto/hash/poseidon2/generate.go
@@ -14,10 +14,7 @@ func Generate(conf config.Curve, baseDir string, bgen *bavard.BatchGenerator) er
 		// {File: filepath.Join(baseDir, "doc.go"), Templates: []string{"doc.go.tmpl"}},
 		{File: filepath.Join(baseDir, "poseidon2.go"), Templates: []string{"poseidon2.go.tmpl"}},
 		{File: filepath.Join(baseDir, "poseidon2_test.go"), Templates: []string{"poseidon2.test.go.tmpl"}},
-		// {File: filepath.Join(baseDir, "options.go"), Templates: []string{"options.go.tmpl"}},
 	}
-	// os.Remove(filepath.Join(baseDir, "utils.go"))
-	// os.Remove(filepath.Join(baseDir, "utils_test.go"))
 
 	return bgen.Generate(conf, conf.Package, "./crypto/hash/poseidon2/template", entries...)
 
