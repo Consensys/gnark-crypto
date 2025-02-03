d67ae5dbfb6562e08bc32393723f539ede5b5453
Arya Tabaie <15056835+Tabaie@users.noreply.github.com>
2025-02-03 13:31:11 -0600

    feat: register poseidon2 for bls12377

--- ecc/bls12-377/fr/poseidon2/hash.go
+++ ecc/bls12-377/fr/poseidon2/hash.go
@@ -2,17 +2,24 @@ package poseidon2
 
 import (
 	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
-	"github.com/consensys/gnark-crypto/hash"
+	gnarkHash "github.com/consensys/gnark-crypto/hash"
+	"hash"
 )
 
 // NewPoseidon2 returns a Poseidon2 hasher
 // TODO @Tabaie @ThomasPiellard Generify once Poseidon2 parameters are known for all curves
-func NewPoseidon2() hash.StateStorer {
-	return hash.NewMerkleDamgardHasher(
+func NewPoseidon2() gnarkHash.StateStorer {
+	return gnarkHash.NewMerkleDamgardHasher(
 		&Hash{params: parameters{
 			t:         2,
 			rF:        6,
 			rP:        26,
 			roundKeys: InitRC("Poseidon2 hash for BLS12_377 with t=2, rF=6, rP=26, d=17", 6, 26, 2),
 		}}, make([]byte, fr.Bytes))
+}
+
+func init() {
+	gnarkHash.RegisterHash(gnarkHash.POSEIDON2_BLS12_377, func() hash.Hash {
+		return NewPoseidon2()
+	})
 }
