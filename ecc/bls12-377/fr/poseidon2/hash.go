5e06e92dfa8efec7f4cc0319573800613094b1c8
Arya Tabaie <15056835+Tabaie@users.noreply.github.com>
2025-02-12 23:32:45 -0600

    revert: make params private again

--- ecc/bls12-377/fr/poseidon2/hash.go
+++ ecc/bls12-377/fr/poseidon2/hash.go
@@ -2,6 +2,7 @@ package poseidon2
 
 import (
 	"hash"
+	"sync"
 
 	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
 	gnarkHash "github.com/consensys/gnark-crypto/hash"
@@ -12,17 +13,17 @@ import (
 func NewMerkleDamgardHasher() gnarkHash.StateStorer {
 	// TODO @Tabaie @ThomasPiellard Generify once Poseidon2 parameters are known for all curves
 	return gnarkHash.NewMerkleDamgardHasher(
-		&Permutation{NewDefaultParameters()}, make([]byte, fr.Bytes))
+		&Permutation{GetDefaultParameters()}, make([]byte, fr.Bytes))
 }
 
-// NewDefaultParameters returns a new set of parameters for the Poseidon2 permutation.
+// GetDefaultParameters returns a set of parameters for the Poseidon2 permutation.
 // The default parameters are:
 // - width: 2
 // - nbFullRounds: 6
 // - nbPartialRounds: 26
-func NewDefaultParameters() *Parameters {
+var GetDefaultParameters = sync.OnceValue(func() *Parameters {
 	return NewParameters(2, 6, 26)
-}
+})
 
 func init() {
 	gnarkHash.RegisterHash(gnarkHash.POSEIDON2_BLS12_377, func() hash.Hash {
