4a839070fd5050c148a98870a13e7c521fd777a7
Youssef El Housni <youssef.housni21@gmail.com>
2025-02-19 09:22:32 -0500

    fix: mul asm babybear

--- field/babybear/extensions/e2_babybear.go
+++ field/babybear/extensions/e2_babybear.go
@@ -4,8 +4,9 @@ import (
 	fr "github.com/consensys/gnark-crypto/field/babybear"
 )
 
-// Mul sets z to the E2-product of x,y, returns z
-func (z *E2) Mul(x, y *E2) *E2 {
+// mulGenericE2 sets z to the E2-product of x,y, returns z
+// note: do not rename, this is referenced in the x86 assembly impl
+func mulGenericE2(z, x, y *E2) *E2 {
 	var a, b, c fr.Element
 	a.Add(&x.A0, &x.A1)
 	b.Add(&y.A0, &y.A1)
@@ -18,8 +19,9 @@ func (z *E2) Mul(x, y *E2) *E2 {
 	return z
 }
 
-// Square sets z to the E2-product of x,x returns z
-func (z *E2) Square(x *E2) *E2 {
+// squareGenericE2 sets z to the E2-product of x,x returns z
+// note: do not rename, this is referenced in the x86 assembly impl
+func squareGenericE2(z, x *E2) *E2 {
 	var a, b, c fr.Element
 	a.Mul(&x.A0, &x.A1).Double(&a)
 	c.Square(&x.A0)
