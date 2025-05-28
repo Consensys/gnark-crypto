60a5ec4ee1c73c2e20262c67cefa5f868ecf4c98
Thomas Piellard <thomas.piellard@consensys.net>
2025-05-28 10:32:00 +0200

    feat: restore default tests

--- field/koalabear/vortex/merkle_test.go
+++ field/koalabear/vortex/merkle_test.go
@@ -14,49 +14,49 @@ func TestMerkleTree(t *testing.T) {
 
 	posLists := []int{0, 1, 12, 31}
 
-	// t.Run("full-zero-leaves", func(t *testing.T) {
-	// 	assert := require.New(t)
-	// 	leaves := [32]Hash{}
+	t.Run("full-zero-leaves", func(t *testing.T) {
+		assert := require.New(t)
+		leaves := [32]Hash{}
 
-	// 	tree := BuildMerkleTree(leaves[:], nil)
+		tree := BuildMerkleTree(leaves[:], nil)
 
-	// 	for _, pos := range posLists {
+		for _, pos := range posLists {
 
-	// 		proof, err := tree.Open(pos)
-	// 		assert.NoError(err)
+			proof, err := tree.Open(pos)
+			assert.NoError(err)
 
-	// 		err = proof.Verify(pos, leaves[pos], tree.Root(), nil)
-	// 		assert.NoError(err)
-	// 	}
-	// })
+			err = proof.Verify(pos, leaves[pos], tree.Root(), nil)
+			assert.NoError(err)
+		}
+	})
 
-	// t.Run("full-random", func(t *testing.T) {
-	// 	assert := require.New(t)
+	t.Run("full-random", func(t *testing.T) {
+		assert := require.New(t)
 
-	// 	var (
-	// 		// #nosec G404 -- test case generation does not require a cryptographic PRNG
-	// 		rng     = rand.New(rand.NewChaCha8([32]byte{}))
-	// 		modulus = uint32(koalabear.Modulus().Int64())
-	// 	)
+		var (
+			// #nosec G404 -- test case generation does not require a cryptographic PRNG
+			rng     = rand.New(rand.NewChaCha8([32]byte{}))
+			modulus = uint32(koalabear.Modulus().Int64())
+		)
 
-	// 	leaves := [32]Hash{}
-	// 	for i := range leaves {
-	// 		for j := range leaves[i] {
-	// 			leaves[i][j] = koalabear.Element{rng.Uint32N(modulus)}
-	// 		}
-	// 	}
+		leaves := [32]Hash{}
+		for i := range leaves {
+			for j := range leaves[i] {
+				leaves[i][j] = koalabear.Element{rng.Uint32N(modulus)}
+			}
+		}
 
-	// 	tree := BuildMerkleTree(leaves[:], nil)
+		tree := BuildMerkleTree(leaves[:], nil)
 
-	// 	for _, pos := range posLists {
-	// 		proof, err := tree.Open(pos)
-	// 		assert.NoError(err)
+		for _, pos := range posLists {
+			proof, err := tree.Open(pos)
+			assert.NoError(err)
 
-	// 		err = proof.Verify(pos, leaves[pos], tree.Root(), nil)
-	// 		assert.NoError(err)
-	// 	}
+			err = proof.Verify(pos, leaves[pos], tree.Root(), nil)
+			assert.NoError(err)
+		}
 
-	// })
+	})
 
 	t.Run("full-random-sha256", func(t *testing.T) {
 		assert := require.New(t)
