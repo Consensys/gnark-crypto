27e99f3c894cb42de4fad81cb76a4490b68b989d
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-06-25 09:10:43 +0000

    chore: generate

--- ecc/bls12-377/g2_test.go
+++ ecc/bls12-377/g2_test.go
@@ -6,6 +6,7 @@
 package bls12377
 
 import (
+	crand "crypto/rand"
 	"fmt"
 	"math/big"
 	"math/rand/v2"
@@ -689,29 +690,30 @@ func BenchmarkG2AffineBatchScalarMultiplication(b *testing.B) {
 }
 
 func BenchmarkG2JacScalarMultiplication(b *testing.B) {
-
-	var scalar big.Int
-	r := fr.Modulus()
-	scalar.SetString("5243587517512619047944770508185965837690552500527637822603658699938581184513", 10)
-	scalar.Add(&scalar, r)
-
-	var doubleAndAdd G2Jac
-
-	b.Run("double and add", func(b *testing.B) {
-		b.ResetTimer()
-		for j := 0; j < b.N; j++ {
-			doubleAndAdd.mulWindowed(&g2Gen, &scalar)
+	for i := 0; i <= fr.Modulus().BitLen(); i += 8 {
+		bound := new(big.Int).Lsh(big.NewInt(1), uint(i))
+		scalar, err := crand.Int(crand.Reader, bound)
+		if err != nil {
+			b.Fatalf("failed to generate random scalar: %v", err)
 		}
-	})
 
-	var glv G2Jac
-	b.Run("GLV", func(b *testing.B) {
-		b.ResetTimer()
-		for j := 0; j < b.N; j++ {
-			glv.mulGLV(&g2Gen, &scalar)
-		}
-	})
+		var doubleAndAdd G1Jac
+		b.Run(fmt.Sprintf("method=window/scalarwidth=%d", i), func(b *testing.B) {
+			b.ResetTimer()
+			for j := 0; j < b.N; j++ {
+				doubleAndAdd.mulWindowed(&g1Gen, scalar)
+			}
+		})
 
+		var glv G1Jac
+		b.Run(fmt.Sprintf("method=GLV/scalarwidth=%d", i), func(b *testing.B) {
+			b.ResetTimer()
+			for j := 0; j < b.N; j++ {
+				glv.mulGLV(&g1Gen, scalar)
+			}
+		})
+
+	}
 }
 
 func BenchmarkG2AffineCofactorClearing(b *testing.B) {
