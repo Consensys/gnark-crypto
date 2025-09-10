1260d09a6a7d747411375cc4309bd7401595bb0d
VolodymyrBg <aqdrgg19@gmail.com>
2025-09-10 12:34:43 +0300

    chore: clarify PublicKey.SetBytes uses compressed public key (#732)
    Co-authored-by: Ivo Kubjas <ivo.kubjas@consensys.net>

--- ecc/bls24-317/ecdsa/ecdsa.go
+++ ecc/bls24-317/ecdsa/ecdsa.go
@@ -25,7 +25,7 @@ const (
 	sizeFr         = fr.Bytes
 	sizeFrBits     = fr.Bits
 	sizeFp         = fp.Bytes
-	sizePublicKey  = sizeFp
+	sizePublicKey  = bls24317.SizeOfG1AffineCompressed
 	sizePrivateKey = sizeFr + sizePublicKey
 	sizeSignature  = 2 * sizeFr
 )
